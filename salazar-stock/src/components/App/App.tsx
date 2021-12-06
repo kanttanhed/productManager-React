49157356956376634a10919766e6c3770c3e60f7
kanttanhed <plinio.salazar@yahoo.com.br>
2021-12-06 00:57:14 -0200

    shared components

--- salazar-stock/src/components/App/App.tsx
+++ salazar-stock/src/components/App/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
-import Header from './Header'
+import Header from '../Header';
+import Button from '../../shared/Button';
+import Container from '../../shared/Container';
+
+function TestComponent () {
+  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
+}
 
 function App() {
   return (
     <div className="App">
-      <Header title="Salazar Stock" />
+      <Header title="AlgaStock" />
+
+      <Container>
+        <Button
+          onClick={() => window.alert('UIIIU')}
+          appendIcon={<TestComponent />}
+        >
+          Alert
+        </Button>
+      </Container>
     </div>
   );
 }
